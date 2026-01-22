b5a634601433149db0adeb825341f732920cc009
ravisankar666 <ravisankarp666@gmail.com>
2026-01-22 14:44:37 +0530

    add terraform file's

--- jenkins.sh
+++ jenkins.sh
@@ -17,6 +17,7 @@ curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenki
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-21-openjdk -y
 yum install jenkins -y
+yum install jenkins -y
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
