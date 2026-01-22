8fe119f1e0f63e68e857108e0699c3dab5bdf5f6
ravisankar666 <ravisankarp666@gmail.com>
2026-01-22 15:50:42 +0530

    add terraform file's

--- variable.tf
+++ variable.tf
@@ -8,13 +8,13 @@ variable "environment" {
 
 variable "zone_name" {
   type        = string
-  default     = "daws86s.fun"
+  default     = "daws86.fun"
   description = "description"
 }
 
 variable "zone_id" {
   type        = string
-  default     = "Z0948150OFPSYTNVYZOY"
+  default     = "Z0043313JVXOCUVVVC7F"
   description = "description"
 }
 
