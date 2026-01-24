70e063c69eabc854a1b004681d06ff3b1d9d3b5f
ravisankar666 <ravisankarp666@gmail.com>
2026-01-24 23:10:11 +0530

    add some changes

--- jenkins.sh
+++ jenkins.sh
@@ -15,12 +15,16 @@ xfs_growfs /home
 
 # wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 # rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install fontconfig java-21-openjdk -y
 
-sudo curl -L -o /etc/yum.repos.d/jenkins.repo \ https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo curl -L -o /etc/yum.repos.d/jenkins.repo \
+ https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 
-sudo yum install fontconfig java-21-openjdk -y
+
 sudo yum install jenkins -y
 
 
