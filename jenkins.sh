fafc885d93630a939bc4b491001624c1008997f6
ravisankar666 <ravisankarp666@gmail.com>
2026-01-24 22:58:04 +0530

    add some changes

--- jenkins.sh
+++ jenkins.sh
@@ -13,14 +13,18 @@ xfs_growfs /var
 xfs_growfs /home
 
 
-sudo curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2025.key
+# wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo curl -L -o /etc/yum.repos.d/jenkins.repo \ https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+
 sudo yum install fontconfig java-21-openjdk -y
 sudo yum install jenkins -y
 
 
 
-systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
-
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
